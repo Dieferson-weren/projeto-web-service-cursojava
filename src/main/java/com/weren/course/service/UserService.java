d47b54f5649112c122bae1a0e8fdaee0443ed922
Dieferson Weren <dieferson.weren@hotmail.com>
2021-03-12 19:12:49 -0300

    Service layer, component registration

---
+++ src/main/java/com/weren/course/service/UserService.java
@@ -0,0 +1,27 @@
+package com.weren.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.weren.course.entities.User;
+import com.weren.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
