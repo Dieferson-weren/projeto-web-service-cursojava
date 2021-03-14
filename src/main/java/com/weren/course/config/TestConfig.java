700eb12f945cbcc75cf191ff109a0180ca6c7188
Dieferson Weren <dieferson.weren@hotmail.com>
2021-03-14 18:10:27 -0300

    Paymente, one-to-one association

--- src/main/java/com/weren/course/config/TestConfig.java
+++ src/main/java/com/weren/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+
 import com.weren.course.entities.Category;
 import com.weren.course.entities.Order;
 import com.weren.course.entities.OrderItem;
+import com.weren.course.entities.Payment;
 import com.weren.course.entities.Product;
 import com.weren.course.entities.User;
 import com.weren.course.entities.enums.OrderStatus;
@@ -79,6 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 }
